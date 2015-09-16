3f9314682ceaffa65bb3bdfd3cde3ec30faee5be
janejanejane <jeanclaudetteambait@gmail.com>
2015-09-16 19:49:29 +0800

    Increase count from previous db log

--- www/js/app.js
+++ www/js/app.js
@@ -31,7 +31,7 @@ angular.module('homage', ['ionic', 'firebase', 'restangular'])
     console.log('i am clicked', $scope.counter);
     console.log('today is', $scope.today);
     var data = {
-      userid: 1,
+      userId: 1,
       date: $scope.today,
       count: $scope.counter
     }
@@ -46,14 +46,30 @@ angular.module('homage', ['ionic', 'firebase', 'restangular'])
       return $http.get('/data/responses.data.json');
     },
     setClickCount: function(click) {
-      $rootScope.clickers.child(click.date)
-        .once('value', function(snapshot) {
-          $rootScope.clickers.set({
-            userid: click.userid,
-            date: click.date,
-            count: click.count
-          })
-        });
+      var userKey = 'user-' + click.userId,
+          thisClick = {};
+      thisClick[click.date] = {};
+
+      // check if date today is logged in db
+      $rootScope.clickers.child(click.date).once('value', function(snapshot){
+        if(snapshot.val() === null) {
+          console.log('snapshot', snapshot.val());
+          thisClick[click.date][userKey] = { count: click.count };
+        } else {
+          var userRecord = snapshot.val()[userKey],
+              totalCount = click.count;
+
+          // check if userId is logged under date today
+          if (userRecord) {
+            totalCount += userRecord.count;
+          }
+
+          thisClick[click.date][userKey] = { count: totalCount };
+        }
+
+        // log to db
+        $rootScope.clickers.set(thisClick);
+      });
     }
   };
 });
