9d0ecd01447b2eb8ab0ab1a567faf9309e4c7c97
janejanejane <jeanclaudetteambait@gmail.com>
2015-09-16 10:15:34 +0800

    Show app text response every click

--- www/js/app.js
+++ www/js/app.js
@@ -11,39 +11,45 @@ angular.module('homage', ['ionic', 'firebase', 'restangular'])
 .controller('HomageCtrl', function($scope, HomageFactory) {
   $scope.counter = 0;
   $scope.today = moment().format('MM-DD-YYYY');
-  $scope.responses = HomageFactory.getAllResponses();
   $scope.shout = '';
+
+  var index = 0;
+
+  HomageFactory.getAllResponses().success(function(data) {
+    console.log('data is', data);
+    $scope.responsedata = data;
+  });
+
+  $scope.displayResponse = function() {
+    console.log('responsedata', $scope.responsedata.responses);
+    index = Math.floor(Math.random() * $scope.responsedata.responses.length);
+    $scope.shout = $scope.responsedata.responses[index];
+  };
 
   $scope.buttonClick = function() {
     $scope.counter++;
     console.log('i am clicked', $scope.counter);
     console.log('today is', $scope.today);
     var data = {
+      userid: 1,
       date: $scope.today,
       count: $scope.counter
     }
     HomageFactory.setClickCount(data);
     $scope.displayResponse();
   };
-
-  $scope.displayResponse = function() {
-    console.log('responses', $scope.responses);
-    $scope.shout = '';
-  };
 })
 
 .factory('HomageFactory', function($rootScope, $http, Restangular) {
   return {
     getAllResponses: function() {
-      $http.get('/data/responses.data.json').success(function(data) {
-        console.log('data is', data);
-        return data.responses;
-      });
+      return $http.get('/data/responses.data.json');
     },
     setClickCount: function(click) {
       $rootScope.clickers.child(click.date)
         .once('value', function(snapshot) {
           $rootScope.clickers.set({
+            userid: click.userid,
             date: click.date,
             count: click.count
           })
