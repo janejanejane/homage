eea44c952cd4d2cc169e89efaa63cab5cd3c821d
janejanejane <jeanclaudetteambait@gmail.com>
2015-09-28 08:02:06 +1300

    Add chart files

---
+++ www/js/controllers/chartctrl.js
@@ -0,0 +1,8 @@
+app
+	.controller('ChartCtrl', function($scope) {
+		var width = window.height,
+			height = window.width,
+			chart = d3.select("#chart-div").append("svg")
+						.attr("width", )
+						.attr("height", );
+	});
