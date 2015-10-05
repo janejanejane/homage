6946d86bba8a355062df7e3ed5590fc2e5972d28
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-10-05 14:24:44 +1300

    Change chart from div to svg

--- www/js/directives/chart.directive.js
+++ www/js/directives/chart.directive.js
@@ -23,71 +23,88 @@ app
 							chart = d3.select("#" + elm[0].id + ' div');
 
 
-							axis = d3.svg.axis()
-									.scale(x)
-									.orient("bottom");
+							// axis = d3.svg.axis()
+							// 		.scale(x)
+							// 		.orient("bottom");
 
-							// reference all divs
-							div = chart.selectAll("div")
-									.data(data, function(d) {
-										console.log('d', d);
-										return d.count + d.date;
-									});
+							// // reference all divs
+							// div = chart.selectAll("div")
+							// 		.data(data, function(d) {
+							// 			console.log('d', d);
+							// 			return d.count + d.date;
+							// 		});
 
-							// remove old
-							div.exit()
-								.remove();
+							// // remove old
+							// div.exit()
+							// 	.remove();
 
-							// add new
-							div.enter()
-								.append("div");
+							// // add new
+							// div.enter()
+							// 	.append("div");
 
-							// update existing
-							div.attr("id", function(d) {
-									return d.date.replace(/[-]/g, '');
-								})
-								.style("height", function(d) { 
-									return scale(d.count) + 10 + "px"; 
-								})
-								.text(function(d) { 
-									return d.count; 
-								});
+							// // update existing
+							// div.attr("id", function(d) {
+							// 		return d.date.replace(/[-]/g, '');
+							// 	})
+							// 	.style("height", function(d) { 
+							// 		return scale(d.count) + 10 + "px"; 
+							// 	})
+							// 	.text(function(d) { 
+							// 		return d.count; 
+							// 	});
 
-							// chart.append("g")
-							// 	.attr("class", "x axis")
-							// 	.attr("transform", "translate(0, 300)")
-							// 	.call(axis);
+							// // chart.append("g")
+							// // 	.attr("class", "x axis")
+							// // 	.attr("transform", "translate(0, 300)")
+							// // 	.call(axis);
 
-						var svgContainer = d3.select("#" + elm[0].id).append("svg"),
+						var svgContainer = d3.select("#" + elm[0].id).append("svg")
+												.attr("width", elm[0].offsetWidth - 20)
+												.attr("height", elm[0].offsetHeight + 70)
+											.append("g")
+												.attr("transform", "translate("+(elm[0].offsetLeft + 20)+", "+(elm[0].offsetTop - 100)+")"),
 							minDate = new Date(val[0]['date']),
-							maxDate = new Date(val[val.length-1]['date']),
+							maxDate = new Date(),
 
 						//Create the Scale we will use for the Axis
 							axisScale = d3.time.scale()
 								.domain([minDate, maxDate])
-								.range([0, 300]),
+								.range([0,  elm[0].offsetWidth - 20]),
 
 						//Create the Axis
 							xAxis = d3.svg.axis()
 								.scale(axisScale)
 								.orient("bottom")
-								.tickPadding(8)
 								.tickFormat(d3.time.format("%b %d")),
 
 						//Create an SVG group Element for the Axis elements and call the xAxis function
 							xAxisGroup = svgContainer.append("g")
 								.attr("class", "axis-date")
+								.attr("transform", "translate(0, 300)")
 								.call(xAxis);
 							
 							// http://bl.ocks.org/phoebebright/3059392
 							svgContainer.selectAll(".axis-date text")  // select all the text elements for the axis-date
 								.attr("transform", function(d) {
-									return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
+									return "translate(" + this.getBBox().height*-0.5 + "," + this.getBBox().height + ")rotate(-60)";
 								});
 
-						console.log(minDate, maxDate);
-
-						console.log('width', width, 'height', height);
+							svgContainer.selectAll("rect")
+								.data(data, function(d) {
+									return d.count + d.date
+								})
+								.enter()
+								.append("rect")
+								.attr("x", function(d) { // placement of bar horizontally
+									return axisScale(new Date(d.date));
+								})
+								.attr("y", function(d) { // stick bar to x-axis
+									return 300 - (scale(d.count) + 10);
+								})
+								.attr("width", 25) // width of individual bars
+								.attr("height", function(d) { // height of individual bars inside chart
+									return scale(d.count) + 10;
+								});
 					}
 				}, true);
 			}
