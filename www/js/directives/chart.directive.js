6d88947200b4ff37238d91e0185b2c18f7de735b
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-10-05 22:00:09 +1300

    Group x-axis and bars correctly

--- www/js/directives/chart.directive.js
+++ www/js/directives/chart.directive.js
@@ -9,87 +9,58 @@ app
 					console.log('val', val, 'elm.id', elm[0].id);
 
 					if(!!val.length) { // length greater than 0
-						var width = $window.innerWidth - 20,
-							height = $window.innerHeight,
-							scale = d3.scale.linear()
+
+						var clicksChartEl = elm[0], // this container
+							width = clicksChartEl.offsetWidth - 40,
+							height = clicksChartEl.offsetHeight,
+							pad = 30,
+							left = clicksChartEl.offsetLeft,
+							top = clicksChartEl.offsetTop - 100,
+
+							// create scale for data count (how tall)
+							y = d3.scale.linear()
 										.domain([0, d3.max(val, function(d) {
 											return d.count;
 										})])
-										.range([0, 300]),
+										.range([0, height]),
+
+							// create scale for data dates (how wide)
 							x = d3.scale.linear()
 									.domain([0, val.length])
-									.range([0, 300]),
-							data = val,
-							chart = d3.select("#" + elm[0].id + ' div');
+									.range([height, 0]),
 
+							// create svg element as container
+							svg = d3.select("#" + elm[0].id).append("svg")
+												.attr("width", width + pad * 2)
+												.attr("height", height + pad * 2),
 
-							// axis = d3.svg.axis()
-							// 		.scale(x)
-							// 		.orient("bottom");
-
-							// // reference all divs
-							// div = chart.selectAll("div")
-							// 		.data(data, function(d) {
-							// 			console.log('d', d);
-							// 			return d.count + d.date;
-							// 		});
-
-							// // remove old
-							// div.exit()
-							// 	.remove();
-
-							// // add new
-							// div.enter()
-							// 	.append("div");
-
-							// // update existing
-							// div.attr("id", function(d) {
-							// 		return d.date.replace(/[-]/g, '');
-							// 	})
-							// 	.style("height", function(d) { 
-							// 		return scale(d.count) + 10 + "px"; 
-							// 	})
-							// 	.text(function(d) { 
-							// 		return d.count; 
-							// 	});
-
-							// // chart.append("g")
-							// // 	.attr("class", "x axis")
-							// // 	.attr("transform", "translate(0, 300)")
-							// // 	.call(axis);
-
-						var svgContainer = d3.select("#" + elm[0].id).append("svg")
-												.attr("width", elm[0].offsetWidth - 20)
-												.attr("height", elm[0].offsetHeight + 70)
-											.append("g")
-												.attr("transform", "translate("+(elm[0].offsetLeft + 20)+", "+(elm[0].offsetTop - 100)+")"),
+							// minimum and maximum dates for use in scaling time
 							minDate = new Date(val[0]['date']),
 							maxDate = new Date(),
 
-						//Create the Scale we will use for the Axis
+							// create scale to use for axis
 							axisScale = d3.time.scale()
 								.domain([minDate, maxDate])
-								.range([0,  elm[0].offsetWidth - 20]),
+								.range([0,  width]),
 
-						//Create the Axis
+							// create every axis point
 							xAxis = d3.svg.axis()
 								.scale(axisScale)
 								.orient("bottom")
-								.tickFormat(d3.time.format("%b %d")),
+								.tickFormat(d3.time.format("%b %d"));
 
-						//Create an SVG group Element for the Axis elements and call the xAxis function
-							xAxisGroup = svgContainer.append("g")
+							// create group for axis and call all axis
+							xAxisGroup = svg.append("g")
 								.attr("class", "axis-date")
-								.attr("transform", "translate(0, 300)")
+								.attr("transform", "translate(0, "+height+")")
 								.call(xAxis);
-							
-							// http://bl.ocks.org/phoebebright/3059392
-							svgContainer.selectAll(".axis-date text")  // select all the text elements for the axis-date
-								.attr("transform", function(d) {
-									return "translate(" + this.getBBox().height*-0.5 + "," + this.getBBox().height + ")rotate(-60)";
-								});
 
-							svgContainer.selectAll("rect")
+							// create group to use for bars
+							rectGroup = svg.append("g")
+											.attr("class", "rect-group");
+
+							// create every single bar
+							rectGroup.selectAll("rect")
 								.data(data, function(d) {
 									return d.count + d.date
 								})
@@ -99,12 +70,36 @@ app
 									return axisScale(new Date(d.date));
 								})
 								.attr("y", function(d) { // stick bar to x-axis
-									return 300 - (scale(d.count) + 10);
+									return height - (y(d.count));
 								})
 								.attr("width", 25) // width of individual bars
 								.attr("height", function(d) { // height of individual bars inside chart
-									return scale(d.count) + 10;
+									return y(d.count);
 								});
+
+							// append text to rect-group
+							rectGroup.selectAll("text")
+								.data(data)
+								.enter()
+								.append("text")
+								.text(function(d) {
+									return d.count;
+								})
+								.attr("x", function(d) { // placement of bar horizontally
+									return axisScale(new Date(d.date)) + 12.5; // half of width: 25
+								})
+								.attr("y", function(d) { // stick bar to x-axis
+									return height - (y(d.count) - 12);
+								})
+								.attr("class", "bar-label");
+
+							// http://bl.ocks.org/phoebebright/3059392
+							// change axis text display
+							svg.selectAll(".axis-date text")  // select all the text elements for the axis-date
+								.attr("transform", function(d) {
+									return "translate(" + this.getBBox().height*-0.1 + "," + (this.getBBox().height+ 10) + ")rotate(-90)";
+								});
+
 					}
 				}, true);
 			}
