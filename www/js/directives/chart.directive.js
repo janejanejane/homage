855b3cf0283e4b23cb0acd9d92a0b599967687cc
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-10-15 16:03:08 +1300

    [Refactor] Separate chart draw and init variables; handle zero clicks data

--- www/js/directives/chart.directive.js
+++ www/js/directives/chart.directive.js
@@ -11,113 +11,130 @@ app
 			},
 			link: function(scope, elm, attrs) {
 
-				function drawChart(val) {
+				var width,
+					height,
+					format,
+					minDate,
+					maxDate,
+					x,
+					y,
+					xAxis,
+					yAxis,
+					line,
+					svg;
+
+				function init() {
 					d3.select("#" + elm[0].id + " svg").remove();
+					var width = elm[0].offsetWidth;
+						height = elm[0].offsetHeight;
 
-					var width = elm[0].offsetWidth,
-						height = elm[0].offsetHeight,
+						format = d3.time.format("%m-%d-%Y").parse;
 
-						format = d3.time.format("%m-%d-%Y").parse,
-
-
-						minDate = moment().subtract(scope.maxDays, 'day'),
-						maxDate = moment(),
+						minDate = moment().subtract(scope.maxDays, 'day');
+						maxDate = moment();
 
 						x = d3.time.scale()
-								.range([0, width - 20]),
+								.range([0, width - 20]);
 
 						y = d3.scale.linear()
-								.range([height, 0])
-								.domain([0, d3.max(val, function(d) {
-									return d.$value;
-								})]),
+								.range([height, 0]);
 
 						xAxis = d3.svg.axis()
 								.scale(x)
 								.orient("bottom")
 								.ticks(d3.time.day, 1)
-								.tickFormat(d3.time.format("%b %d")),
+								.tickFormat(d3.time.format("%b %d"));
 
 						yAxis = d3.svg.axis()
 								.scale(y)
 								.orient("left")
-								.ticks(5),
+								.ticks(5);
 
 						line = d3.svg.line()
 								.x(function(d) { 
 									return x(new Date(d.$id)); 
 								})
 								.y(function(d) { 
-									return y(d.$value); 
-								}),
+									return y(d.$value);
+								});
 
 						svg = d3.select(elm[0]).append("svg")
 									.attr("width", width - 20)
-									.attr("height", height + 20)
-								.append("g")
-									.attr("transform", "translate(30,10)scale(0.9)");
+									.attr("height", height + 20);
+				};
 
-						if(val.length > 7) {
-							minDate = moment().subtract(30, 'day');
-							maxDate = moment();
-							xAxis.ticks(d3.time.day, 3);
-						}
+				function drawChart(val) {
+					d3.select("#" + elm[0].id + " svg g").remove();
 
-						x.domain([minDate, maxDate]);
+					if(val.length > 7) {
+						minDate = moment().subtract(30, 'day');
+						maxDate = moment();
+						xAxis.ticks(d3.time.day, 3);
+					}
 
+					x.domain([minDate, maxDate]);
+					y.domain([0, d3.max(val, function(d) {
+						return (d.$value < 5) ? 4 : d.$value;
+					})]);
 
-						svg.append("g")
-							.attr("class", "x axis")
-							.attr("transform", "translate(0," + height + ")")
-							.call(xAxis);
+					var group = svg.append("g")
+								.attr("transform", "translate(30,10)scale(0.9)");
 
-						// @link: http://bl.ocks.org/phoebebright/3059392
-						// change axis text display
-						svg.selectAll(".x.axis text")  // select all the text elements for the axis-date
-							.attr("transform", function(d) {
-								return "translate(" + this.getBBox().height*-0.3 + "," + (this.getBBox().height) + ")rotate(-45)";
-							});
+					group.append("g")
+						.attr("class", "x axis")
+						.attr("transform", "translate(0," + height + ")")
+						.call(xAxis);
 
-						svg.append("g")
-								.attr("class", "y axis")
-								.call(yAxis)
-							.append("text")
-								.attr("transform", "rotate(-90)")
-								.attr("y", 6)
-								.attr("dy", ".71em")
-								.style("text-anchor", "end")
-								.text("Count");
+					// @link: http://bl.ocks.org/phoebebright/3059392
+					// change axis text display
+					group.selectAll(".x.axis text")  // select all the text elements for the axis-date
+						.attr("transform", function(d) {
+							return "translate(" + this.getBBox().height*-0.3 + "," + (this.getBBox().height) + ")rotate(-45)";
+						});
 
-						svg.append("path")
-								.datum(val)
-								.attr("class", "line")
-								.attr("d", line);
+					group.append("g")
+							.attr("class", "y axis")
+							.call(yAxis)
+						.append("text")
+							.attr("transform", "rotate(-90)")
+							.attr("y", 6)
+							.attr("dy", ".71em")
+							.style("text-anchor", "end")
+							.text("Count");
 
-						svg.selectAll("circle")
-							.data(val, function(d) {
-								return d.$value + d.$id;
-							})
-							.enter().append("circle")
-							.attr("cy", function(d) {
-								return y(d.$value);
-							})
-							.attr("cx", function(d) {
-								return x(new Date(d.$id));
-							})
-							.attr("r", function() {
-								return 2;
-							})
-							.attr("class", "click-circle");
+					group.append("path")
+							.datum(val)
+							.attr("class", "line")
+							.attr("d", line);
+
+					var circles = group.append("g")
+									.attr("class", "circle-group");
+
+					circles.selectAll("circle")
+						.data(val, function(d) {
+							return d.$value + d.$id;
+						})
+						.enter().append("circle")
+						.attr("cy", function(d) {
+							return y(d.$value);
+						})
+						.attr("cx", function(d) {
+							return x(new Date(d.$id));
+						})
+						.attr("r", function() {
+							return 2;
+						})
+						.attr("class", "click-circle");
 				}
-
 
 				scope.$watchCollection('clickArray', function(val) {
 					if(val && !!val.length) { // is not undefined and length greater than 0
-						// initial render
+						// initialize variables
+						init();
+						// render
 						drawChart(val);
 					}
 				});
-
 
 				scope.$watch('choice', function(val) {
 					if(val === 'month') {
@@ -130,6 +147,7 @@ app
 				// bind draw action on screen resize then call angular digest
 				angular.element($window).bind('resize', function() {
 					if(scope.clickArray && !!scope.clickArray.length) {
+						init();
 						scope.$apply(drawChart(scope.clickArray));
 					}
 				});
