657cdcb75c872b847fe5743a6f7ee0a40afacbf1
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-10-23 11:17:45 +1300

    Add achievement logs to db

--- www/js/services/homage.factory.js
+++ www/js/services/homage.factory.js
@@ -1,5 +1,5 @@
 app
-  .factory('HomageFactory', ['$firebaseArray', '$firebaseObject', '$http', 'FIREBASE_URL', 'Restangular', function($firebaseArray, $firebaseObject, $http, FIREBASE_URL, Restangular) {
+  .factory('HomageFactory', ['$firebaseArray', '$firebaseObject', '$http', 'FIREBASE_URL', 'Restangular', 'AchievementFactory', function($firebaseArray, $firebaseObject, $http, FIREBASE_URL, Restangular, AchievementFactory) {
     var ref = new Firebase(FIREBASE_URL);
 
     Restangular.baseUrl = FIREBASE_URL;
@@ -19,13 +19,48 @@ app
         obj.set({
           clicks: [],
           name: uuid,
-          totalCount: 0
+          totalCount: 0,
+          achievements: []
         });
       },
       setClickCount: function(uuid, dateString, value){
         var obj = ref.child('clickerz/'+uuid+'/clicks/'+dateString);
         obj.set(value, function(){
           console.log('Done setting to database');
+          switch (value) {
+            case 5:
+              AchievementFactory.onUnlocked(uuid, '5 clicks!');
+              break;
+            case 10:
+              AchievementFactory.onUnlocked(uuid, '10 clicks!');
+              break;
+            case 20:
+              AchievementFactory.onUnlocked(uuid, '20 clicks!');
+              break;
+            case 50:
+              AchievementFactory.onUnlocked(uuid, '50 clicks!');
+              break;
+            case 100:
+              AchievementFactory.onUnlocked(uuid, '100 clicks!');
+              break;
+            case 250:
+              AchievementFactory.onUnlocked(uuid, '250 clicks!');
+              break;
+            case 500:
+              AchievementFactory.onUnlocked(uuid, '500 clicks!');
+              break;
+            case 1000:
+              AchievementFactory.onUnlocked(uuid, '1000 clicks!');
+              break;
+            case 2000:
+              AchievementFactory.onUnlocked(uuid, '2000 clicks!');
+              break;
+            case 5000:
+              AchievementFactory.onUnlocked(uuid, '5000 clicks!');
+              break;
+            default:
+              break;
+          }
         });
         this.setTotalCount(uuid, value);
       },
