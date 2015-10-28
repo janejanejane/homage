36a9fc18d4e9697ab5c463466a2bd6c35fc4053b
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-10-29 10:22:21 +1300

    Change achievements layout to list

--- www/js/directives/achievement.directive.js
+++ www/js/directives/achievement.directive.js
@@ -4,11 +4,30 @@ app
 			restrict: 'E',
 			replace: true,
 			scope: {
-				achievementsDeclared: '='
+				achievementsDeclared: '=',
+				achievementArray: '='
 			},
 			link: function(scope, elm, attrs) {
-				scope.$watchCollection('achievementsDeclared', function(val) {
-					console.log('achievementsDeclared', val);
+
+				var record = null;
+
+				scope.$watchCollection('achievementArray', function(val) {
+					console.log('achievementArray', val, scope.list);
+					scope.list = [];
+					for (var i = 0; i < scope.achievementsDeclared.length; i++) {
+						// copy details
+						record = scope.achievementsDeclared[i];
+
+						for (var j = 0; j < val.length; j++) {
+							if(scope.achievementsDeclared[i].name === val[j].name) {
+								record['unlocked'] = true;
+							}
+						};
+
+						// add to new array
+						scope.list.push(record);
+					};
+					console.log(scope.list);
 				});
 
 				scope.obj = function(val) {
@@ -20,8 +39,13 @@ app
 				};
 			},
 			template:
-				'<div>'+
-					'<img ng-repeat="i in achievementsDeclared track by $index" class="item-image" src="img/30x30.jpg" ng-click="showDetails(i.description)">'+
+				'<div class="list">'+
+					'<div ng-repeat="i in list track by $index">'+
+						'<div class="achievement-list">'+
+							'<img class="item-image" ng-src="{{!i.unlocked && \'img/fff.png\' || \'img/30x30.jpg\'}}" ng-click="showDetails(i.description)">'+
+							'<h6>{{i.unlocked && i.description || \'???\'}}</h6>'+
+						'</div>'+
+					'</div>'+
 				'</div>'
 		}
 	}]);
