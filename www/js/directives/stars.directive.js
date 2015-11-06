17419587bc4f128bb0d3affc7bc8575da904250c
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-11-06 14:48:34 +1300

    Add small center circle surrounded with bigger circles

--- www/js/directives/stars.directive.js
+++ www/js/directives/stars.directive.js
@@ -15,10 +15,11 @@ app
 
 					var width = elm.parent().prop('offsetWidth'),
 						height = elm.parent().prop('offsetHeight'),
-						circles = [];
+						circles = [],
+						total = 15;
 
-					for (var i = 0; i < 10; i++) {
-						circles.push(Math.random() * 100);
+					for (var i = 0; i < total; i++) {
+						circles.push(Math.round(Math.random() * 100));
 					};
 
 					var svg = d3.select(elm[0])
@@ -28,52 +29,43 @@ app
 						.append("g")
 						.attr("transform", "translate(0, 20)");
 
-					svg.selectAll("circle")
-						.data(circles)
-						.enter().append("circle")
+					var center = svg.append("circle")
 						.style("stroke", "gray")
 						.style("fill", "blue")
-						.attr("r", 20)
-						.attr("cx", function(d) {
-							console.log("cx", d);
-							return (width / 2);
+						.attr("r", 5)
+						.attr("cx", function() {
+							return ((width - 20) / 2);
 						})
-						.attr("cy", function(d) {
-							console.log("cy", d);
+						.attr("cy", function() {
 							return (height / 3);
-						})
-						.transition()
-						.duration(1000)
-						.delay(function(d,i) {
-							return i * 500;
-						})
-						.attr("cx", function(d) {
-							console.log("cx", d);
-							if(Math.round(d) % 2 === 0) {
-								return (width / 2) - d;
-							} else {
-								return (width / 2) + d;
-							}
-						})
-						.attr("cy", function(d) {
-							console.log("cy", d);
-							return (height / 3) - d;
-						})
-						.transition()
-						.duration(1000)
-						.attr("cx", function(d) {
-							console.log("cx", d);
-							if(Math.round(d) % 2 === 0) {
-								return (width / 2) - (d * 2);
-							} else {
-								return (width / 2) + (d * 2);
-							}
-						})
-						.attr("cy", function(d) {
-							console.log("cy", d);
-							return height;
 						});
 
+					var outside = svg.append("g")
+						.attr("transform", "translate("+ ((width - 20) / 2) +", "+ (height / 3) +")");
+
+					var circleAngle = d3.scale.linear().range([0,360]).domain([0,total]);
+
+					outside.selectAll("circle")
+						.data(circles)
+						.enter().append("circle")
+						.style("stroke", "red")
+						.style("fill", "black")
+						.attr("r", 10)
+						.attr("cx", 10)
+						.attr("cy", function(d, i) {
+							return i;
+						})
+						.attr("transform", function(d, i) {
+							return "rotate("+ circleAngle(i) +", 0, 0)";
+						// })
+						// .transition()
+						// .duration(1000)
+						// .attr("transform", function(d, i) {
+						// 	var x = (i > total/2) ? circleAngle(i) * 10 : (-circleAngle(i)) * 10;
+						// 	var y = (i > total/2) ? circleAngle(i) * 10 : (-circleAngle(i)) * 10;
+						// 	console.log("x", x, "y", y);
+						// 	return "translate("+ x +", "+ y +")";
+						});
 				});
 			}	
 		}
