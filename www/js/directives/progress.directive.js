cd41e51ba1efad1a49a84b53cdd0babdec949747
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2015-11-12 09:50:10 +1300

    Change progress to circle behind click image

--- www/js/directives/progress.directive.js
+++ www/js/directives/progress.directive.js
@@ -8,50 +8,73 @@ app
 			},
 			replace: true,
 			link: function(scope, elm, attrs) {
+				var width = elm[0].offsetWidth,
+					height = 250,
+					twoPi = 2 * Math.PI;
+
+				var svg = d3.select(elm[0])
+					.append("svg")
+					.attr("class", "progress-level");
+
+				var group = svg.append("g")
+					.attr("transform", "translate(" + (width / 2) + ", " + (height / 2) + ")");
+
+				var arc = d3.svg.arc()
+					.startAngle(0)
+					.innerRadius(0)
+					.outerRadius(120);
+
+				// progress background
+				var base = group.append("g")
+					.attr("id", "progress-bar");
+
+				var progressCircle = base.append("path")
+					.datum({endAngle: twoPi})
+					.attr("d", arc);
+
+				// draw the progress
+				var progress = base.append("path")
+					.datum({endAngle: 0})
+					.attr("fill", "#990100")
+					.attr("d", arc);
+
+				// angle of progress
+				var x = d3.scale.linear();
 
 				function drawProgress(val) {
 
-						d3.select("#" + elm[0].id + " svg").remove();
-
-						var width = elm[0].offsetWidth;
+						width = elm[0].offsetWidth;
 						scope.currentLevel = Math.floor(Math.log(val)/Math.LN2);
 
-						var svg = d3.select(elm[0])
-							.append("svg")
-							.attr("class", "progress-level")
+						// update domain of values for progress angle
+						x.domain([scope.currentLevel, scope.currentLevel + 1])
+							.range([0, twoPi]);
+
+						// select existing svg
+						svg = d3.select(elm[0])
+							.select("svg")
 							.attr("width", width)
-							.attr("height", 30)
-							.append("g")
-							.attr("transform", "translate(0, 10)");
+							.attr("height", height);
 
-						var x = d3.scale.linear()
-								.domain([scope.currentLevel, scope.currentLevel + 1])
-								.range([0, width]);
+						// update group placement
+						group.attr("transform", "translate(" + (width / 2) + ", " + (height / 2) + ")");
 
-						svg.append("text")
-							.text("Lvl " + scope.currentLevel)
-							.attr("id", "current-level");
+						// add color to the circle background
+						progressCircle.attr("fill", "#333333");
 
-						svg.append("text")
-							.text("Lvl " + (scope.currentLevel + 1))
-							.attr("x", width - 35)
-							.attr("id", "next-level");
-
-						svg.append("rect")
-							.attr("width", width)
-							.attr("height", 10)
-							.attr("x", 0)
-							.attr("y", 5)
-							.attr("fill", "#FF6347");
-
-						svg.append("rect")
-							.attr("width", function() {
-								return x(Math.log(val)/Math.LN2);
-							})
-							.attr("height", 10)
-							.attr("x", 0)
-							.attr("y", 5)
-							.attr("id", "progress-bar");
+						// change progress angle with tween
+						progress.transition()
+							.ease("elastic")
+							.duration(750)
+							.attrTween("d", function(d) {
+								var interpolate = d3.interpolate(d.endAngle, x(Math.log(val)/Math.LN2));
+								return function(t) {
+									// set for progress end arc
+									d.endAngle = interpolate(t);
+									// compute arc with given value
+									return arc(d);
+								}
+							});
 				};
 
 				scope.$watch('totalClicks', function(val) {
